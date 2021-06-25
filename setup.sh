8eb05378502b57b1e581604504de7b805e59349b
unknown <giovanemiranda360@gmail.com>
2021-06-24 23:32:42 -0300

    setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"giovanemiranda360@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.tomml
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
